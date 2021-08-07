07b237f736f4d3d04438fa70163b2261e76ce079
Mindaugas <darbas.mindaugas@gmail.com>
2021-08-07 13:23:26 +0300

    Adding routing and custom styling for header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { HeaderComponent } from './components/header/header.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent },
+      {path: 'home', component: HomeComponent },
+      {path: 'weather', component: WeatherComponent },
+      {path: '**', redirectTo: 'home', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
